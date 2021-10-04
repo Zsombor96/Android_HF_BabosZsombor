533b53498fe43cc7c9d2ffc7f96697699ac25a5c
Zsombor96 <baboszsombor@gmail.com>
2021-10-04 12:51:31 +0300

    HF2

---
+++ hazi2/app/src/main/java/com/example/hazi2/Activity2.java
@@ -0,0 +1,60 @@
+package com.example.hazi2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),Activity3.class);
+                startActivity(intent);
+            }
+        });
+    }
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Statusz","ActivityTwo: onStart()");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Statusz","ActivityTwo: onResume()");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Statusz","ActivityTwo: onPause()");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Statusz","ActivityTwo: onStop()");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("Statusz","ActivityTwo: onDestroy()");
+    }
+}
